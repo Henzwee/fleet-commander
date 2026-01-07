51e663b7ca29b7997e415c2e110876629050fe06
Henry Seal <thehenryseal@gmail.com>
2026-01-07 01:51:27 +0000

    File changes

--- src/components/game/ShipImages.jsx
+++ src/components/game/ShipImages.jsx
@@ -5,7 +5,11 @@ export const SHIP_IMAGES = {
     'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695af5ca435140b76c0dadc9/aaa217e4e_unregistered2.png',
     'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695af5ca435140b76c0dadc9/898048e5d_unregistered3.png'
   ],
-  'Known': [],
+  'Known': [
+    'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695af5ca435140b76c0dadc9/8ede9a6b1_known1.png',
+    'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695af5ca435140b76c0dadc9/b3af572e1_known2.png',
+    'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695af5ca435140b76c0dadc9/fab1a9948_known3.png'
+  ],
   'Notorious': [],
   'Esteemed': [],
   'Renowned': [],
